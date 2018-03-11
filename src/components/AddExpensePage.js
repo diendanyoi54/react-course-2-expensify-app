2955dc3f4b5bd9259cf3d8089358053f66f4e1ac
Noel Hahn <noelhahn@gmail.com>
2018-03-11 12:46:15 -0400

    Setup formatting for amount and createdAt

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -24,9 +24,9 @@ export class AddExpensePage extends React.Component {
 // The commented out lines in this file were the way redux was handled previously.
 // The reason for the updates, i.e. the additional function below, is because
 // a spy can be used when testing for the onSubmit function and not dispatch
-const mapDispatchToProps = (dispatch) => {
-    addExpense: (expense) => dispatch(addExpense(expense));
-}
+const mapDispatchToProps = (dispatch) => ({
+    addExpense: (expense) => dispatch(addExpense(expense))
+})
 
 // export default connect()(AddExpensePage);
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
